39feac6e991936533475b320ee55653d3aef4355
klems <klems@klems.net>
2016-05-14 14:29:02 +0200

    https temporarly removed

--- navbar.php
+++ navbar.php
@@ -7,7 +7,7 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="https://2euros.net">2euros.net</a>
+      <a class="navbar-brand" href="http://2euros.net">2euros.net</a>
     </div>
     <div class="collapse navbar-collapse" id="myNavbar">
       <ul class="nav navbar-nav">
