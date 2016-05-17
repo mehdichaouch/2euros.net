2e4195fa73e5492a14977a5642ffd75195a73cb1
klems <klems@klems.net>
2016-05-17 23:12:48 +0200

    new style for buttons

--- manage.php
+++ manage.php
@@ -210,11 +210,11 @@
 
       <?php
         if(isset($_GET['state'])) {
-          echo '<button type="submit" class="btn btn-default">Cancel</button> ';
-          echo '<button type="submit" class="btn btn-success">Add</button> ';
-          echo '<button type="submit" class="btn btn-danger">Remove</button> ';
+          echo '<button type="submit" class="btn btn-default-outline">Cancel</button> ';
+          echo '<button type="submit" class="btn btn-success-outline">Add</button> ';
+          echo '<button type="submit" class="btn btn-danger-outline">Remove</button> ';
         } else {
-          echo '<button type="submit" class="btn btn-default">Next</button>'; 
+          echo '<button type="submit" class="btn btn-info-outline">Next</button>'; 
         }
       ?>
 
