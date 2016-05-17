af63671e478ed6584461efff4cca08606ee27d06
klems <klems@klems.net>
2016-05-17 21:59:12 +0200

    new behaviour on button

--- manage.php
+++ manage.php
@@ -188,7 +188,7 @@
           // Create connection
           mysql_connect($serverip, $user, $pass, $dbname);
           mysql_select_db('2euros');
-          $sql = "SELECT pic_url FROM Coins WHERE year = '$selectedYear' AND country = '$selectedCountry' AND event = '$selectedEvent';";
+          $sql = "SELECT pic_url FROM Coins WHERE year = '$selectedYear' AND country = '$selectedCountry' AND event like '$selectedEvent';";
           $result = mysql_query($sql);
 
 
