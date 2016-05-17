5b89ec385ca840e44fc1d5bec20f16681fdf0b9f
klems <klems@klems.net>
2016-05-17 20:48:36 +0200

    new behaviour on button

--- manage.php
+++ manage.php
@@ -178,7 +178,7 @@
 
       <?php
         if(isset($_GET['state'])) {
-          $test=woi.png;
+          $test = "woi.png";
           echo '<label for="sel5">Preview </label>';
           echo "<img src=resources/" .$test. " width=50 height=50 class=img-circle></a><br>";
           echo '<br>';
