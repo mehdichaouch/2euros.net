1f2a7cc4361a255673da8c48706f96a535ee9829
clement.tricart <clement.tricart@photobox.com>
2016-05-26 23:13:54 +0200

    progressbar updated

--- progress.php
+++ progress.php
@@ -32,7 +32,7 @@
 <!-- CONTENT -->
 <div class="container">
   <h2>Progress</h2>
-  <p>Track your progress here !!</p>   
+  <p>Track your progress here (results may vary due to DB modifications overtime)</p>   
 
   <!-- <div class="progress">
     <div class="progress-bar progress-bar-info progress-bar-striped active" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width:60%">
