f30ed15ba2cbc56a4843f2f7d45727199426e430
klems <klems@klems.net>
2016-05-29 15:45:24 +0200

    test css

--- collection.php
+++ collection.php
@@ -71,7 +71,7 @@
 
         // output data of each row
         while($row = $result->fetch_assoc()) {
-            echo "<tr style='height:35px; vertical-align:middle;'>";
+            echo "<tr height=35px vertical-align=middle>";
             echo "<td>" . $row["year"]. "</td>";
             echo "<td><img src=resources/coins/" . $row["pic_url"]. " width=50 height=50 class=img-circle></a><br></td>";
             echo "<td>" . $row["country"]. "</td>";
