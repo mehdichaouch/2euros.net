cd1acbd60dc88c9c5f04ae6e43d31cdd8b861ac7
klems <klems@klems.net>
2016-05-29 16:36:43 +0200

    test css

--- progress2.php
+++ progress2.php
@@ -45,7 +45,7 @@
         <div class="progress-bar progress-bar-striped active" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width:40%">40%</div><br>
         </div>
 
-        Total: XX Euros
+        <u>Total:</u> <b>12</b> euros
 
       </div>
     
