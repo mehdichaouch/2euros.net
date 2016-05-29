6554c3cfcc83cf1bd8aac6b5d84bf88df90131cf
klems <klems@klems.net>
2016-05-29 16:38:01 +0200

    test css

--- progress2.php
+++ progress2.php
@@ -36,7 +36,7 @@
     <div class="panel panel-default">
     
       <div class="panel-heading">
-        2005
+        <h4>2005</h4>
       </div>
     
       <div class="panel-body">
@@ -45,7 +45,7 @@
         <div class="progress-bar progress-bar-striped active" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width:40%">40%</div><br>
         </div>
 
-        <u>Total:</u> <b>12</b> euros
+        Total: <b>12</b> euros
 
       </div>
     
