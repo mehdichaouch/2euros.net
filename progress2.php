7333c671a963ce4b6d67c7ca2fa8cc4fccfaa5d0
klems <klems@klems.net>
2016-05-29 16:17:25 +0200

    test css

--- progress2.php
+++ progress2.php
@@ -30,12 +30,19 @@
   ?>
 
   <!-- CONTENT -->
-  <div class="panel-group">
-    <div class="panel panel-default">
-      <div class="panel-body">Panel Content</div>
+  <div class="panel panel-default">
+    <div class="panel-heading">
+      2012
     </div>
-    <div class="panel panel-default">
-      <div class="panel-body">Panel Content</div>
+    <div class="panel-body">
+
+      <div class="progress">
+      <div class="progress-bar progress-bar-info progress-bar-striped active" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width:60%">
+        60% Complete
+      </div>
+      
+    </div>
+
     </div>
   </div>
 
