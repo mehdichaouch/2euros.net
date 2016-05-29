b8edd81ea78259d266fbd52f97cd3719189f0d2f
klems <klems@klems.net>
2016-05-29 15:29:06 +0200

    test css

--- browse.php
+++ browse.php
@@ -17,6 +17,16 @@
   <link rel="apple-touch-icon" sizes="76x76" href="touch-icon-ipad.png">
   <link rel="apple-touch-icon" sizes="120x120" href="touch-icon-iphone-retina.png">
   <link rel="apple-touch-icon" sizes="152x152" href="touch-icon-ipad-retina.png">
+  <style type="text/css">
+  body {
+    td {
+    vertical-align: baseline;
+    }
+    td img {
+    vertical-align: top;
+    }
+  }
+  </style>
 </head>
 
 <body>
