676107e045f09ae2f6693823c6fbe74d667a9cd5
klems <klems@klems.net>
2016-05-29 19:57:50 +0200

    flags fixed

--- progress_country.php
+++ progress_country.php
@@ -81,8 +81,8 @@
         // #--HTML--#
 
         echo '<div class="panel panel-default">';
-        echo '<div class="panel-heading"><b>'. $row['country'] .'</b>&nbsp;';
-        echo '<img src="resources/flags/' . $flag_url . '.png" width=20></a>';
+        echo '<img src="resources/flags/' . $flag_url . '.png" width=20></a> ';
+        echo '<div class="panel-heading"><b>'. $row['country'] .'</b>';
         echo '</div>';
         echo '<div class="panel-body">';
         echo '<div class="progress">';
