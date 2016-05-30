e69d4e3cc340b0ca4feb54b3ee86662558783f8b
klems <klems@klems.net>
2016-05-30 21:59:01 +0200

    new browsing page

--- browse2.php
+++ browse2.php
@@ -64,7 +64,8 @@
 
       echo '<div class="panel-body">';
 
-      echo '<table>';
+      echo '<table class="table table-hover table-striped">';
+      echo '<tbody>';
       echo '<tr>';
       while ($row = mysql_fetch_array($result_all_coins)) {   
         echo '<td>' . $row['year'] . '</td>';
@@ -73,7 +74,8 @@
         echo '<td>' . $row['coinage'] . '</td>';
         echo '<td>' . $row['event'] . '</td>';
       }
-      echo "</tr>";
+      echo '</tr>';
+      echo '<tbody>';
       echo '</table>';
 
       echo '</div>';
